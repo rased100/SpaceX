a135711b5180b62c637dec3497b96f8a52788488
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-25 10:46:55 +0600

    data load from api

--- src/Pages/Home/FetchItems.js
+++ src/Pages/Home/FetchItems.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import ItemsComponent from './ItemsComponent';
 
 const FetchItems = () => {
     const items = useSelector((state) => state);
     // console.log(items);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        fetch(`https://api.spacexdata.com/v3/launches`)
+            .then(res => res.json())
+            .then(data => {
+                console.log('data', data)
+            })
+    }, [])
     return (
         <div>
             <ItemsComponent />
